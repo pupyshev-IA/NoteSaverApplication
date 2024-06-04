3208719db2c55add2c46e571d1e6fb451956954c
Ilya Pupyshev <pupischev.ilja@gmail.com>
2024-06-04 13:44:00 +0300

    Migrator implemented

---
+++ Abdt.Loyal.NoteSaver.Migrator/Program.cs
@@ -0,0 +1,16 @@
+﻿using Abdt.Loyal.NoteSaver.Repository;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+
+var configuration = new ConfigurationBuilder()
+    .AddJsonFile("MigrationSettings.json", false, true)
+    .AddEnvironmentVariables()
+    .Build();
+
+var connectionString = configuration.GetConnectionString("NoteConnectionString");
+
+var optionsBuilder = new DbContextOptionsBuilder<NoteContext>();
+optionsBuilder.UseNpgsql(connectionString);
+
+var context = new NoteContext(optionsBuilder.Options);
+context.Database.Migrate();
