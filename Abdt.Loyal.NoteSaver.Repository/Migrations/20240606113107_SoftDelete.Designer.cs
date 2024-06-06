e41e2d0d207af1a07fad636dca6a001e0344db2d
Ilya Pupyshev <pupischev.ilja@gmail.com>
2024-06-06 14:32:18 +0300

    Migrate SoftDeletion

--- Abdt.Loyal.NoteSaver.Repository/Migrations/20240606113107_SoftDelete.Designer.cs
+++ Abdt.Loyal.NoteSaver.Repository/Migrations/20240606113107_SoftDelete.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Abdt.Loyal.NoteSaver.Repository;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
@@ -11,9 +12,11 @@ using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 namespace Abdt.Loyal.NoteSaver.Repository.Migrations
 {
     [DbContext(typeof(NoteContext))]
-    partial class NoteContextModelSnapshot : ModelSnapshot
+    [Migration("20240606113107_SoftDelete")]
+    partial class SoftDelete
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
