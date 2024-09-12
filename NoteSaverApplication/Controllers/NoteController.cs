e43780d66f4eb269a8b75b38b2ead387a2217973
Pupyshev Ilya <PupyshevIA@abdgtl.com>
2024-09-12 12:25:51 +0300

    Add note controller

---
+++ NoteSaverApplication/Controllers/NoteController.cs
@@ -0,0 +1,106 @@
+﻿using Abdt.Loyal.NoteSaver.BusinessLogic.Abstractions;
+using Abdt.Loyal.NoteSaver.Domain;
+using Abdt.Loyal.NoteSaver.DTO;
+using FluentValidation;
+using Mapster;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Abdt.Loyal.NoteSaver.Controllers
+{
+    [ApiController]
+    [Route("api/v1/notes")]
+    public class NoteController : ControllerBase
+    {
+        private readonly IStorageLogic<Note> _service;
+        private readonly IValidator<Note> _validator;
+
+        public NoteController(IStorageLogic<Note> service, IValidator<Note> validator)
+        {
+            _service = service;
+            _validator = validator;
+        }
+
+        [HttpGet]
+        [Route("{id}")]
+        public async Task<IActionResult> GetNoteById([FromRoute] long id)
+        {
+            var isValidId = id >= 1;
+
+            if (!isValidId)
+                return NotFound();
+
+            var note = await _service.Get(id);
+            var adaptedNote = note.Adapt<NoteDtoOut>();
+
+            return Ok(adaptedNote);
+        }
+
+        [HttpPost]
+        [Route("add")]
+        public async Task<IActionResult> AddNote([FromBody] NoteDto noteDto)
+        {
+            var note = new Note() { Title = noteDto.Title, Content = noteDto.Content };
+
+            if (!_validator.Validate(note).IsValid)
+                return UnprocessableEntity();
+
+            var addedNoteId = await _service.Add(note);
+
+            return Created(new Uri("api/v1/notes/", UriKind.Relative), addedNoteId);
+        }
+
+        [HttpPut]
+        [Route("update")]
+        public async Task<IActionResult> UpdateNote([FromBody] NoteDtoUpdate noteDtoUpdate)
+        {
+            var note = new Note() { Id = noteDtoUpdate.Id, Title = noteDtoUpdate.Title, Content = noteDtoUpdate.Content };
+            var validationResult = _validator.Validate(note);
+            var isValidId = note.Id >= 1;
+
+            if (!validationResult.IsValid || !isValidId)
+                return UnprocessableEntity(validationResult.Errors);
+
+            var updatedNote = await _service.Update(note);
+            var adaptedNote = updatedNote.Adapt<NoteDtoOut>();
+
+            return Ok(adaptedNote);
+        }
+
+        [HttpDelete]
+        [Route("delete")]
+        public async Task<IActionResult> DeleteNote([FromQuery] long id)
+        {
+            var isValidId = id >= 1;
+            if (!isValidId)
+                return NotFound();
+
+            await _service.Delete(id);
+
+            return NoContent();
+        }
+
+        [HttpGet]
+        [Route("list")]
+        public async Task<IActionResult> GetPage([FromQuery] ushort pageNumber, int itemsCount)
+        {
+            var noteList = await _service.GetPage(pageNumber, itemsCount);
+
+            var page = noteList.Adapt<Page<NoteDtoOut>>();
+
+            return Ok(page);
+        }
+
+        [HttpPost]
+        [Route("addTest")]
+        public async Task<IActionResult> AddTestNotes([FromQuery] ushort count)
+        {
+            foreach (var item in Enumerable.Range(0, count))
+            {
+                var note = new Note() { Title = item.ToString(), Content = Guid.NewGuid().ToString() };
+                _ = await _service.Add(note);
+            }
+
+            return NoContent();
+        }
+    }
+}
--- NoteSaverApplication/Program.cs
+++ NoteSaverApplication/Program.cs
@@ -3,11 +3,9 @@ using Abdt.Loyal.NoteSaver.BusinessLogic.Abstractions;
 using Abdt.Loyal.NoteSaver.BusinessLogic.Validation;
 using Abdt.Loyal.NoteSaver.Domain;
 using Abdt.Loyal.NoteSaver.Domain.Options;
-using Abdt.Loyal.NoteSaver.DTO;
 using Abdt.Loyal.NoteSaver.Repository;
 using Abdt.Loyal.NoteSaver.Repository.Abstractions;
 using FluentValidation;
-using Mapster;
 using Microsoft.EntityFrameworkCore;
 using NLog.Web;
 
@@ -34,9 +32,6 @@ builder.Services.AddStackExchangeRedisCache(options =>
     options.InstanceName = redisCacheSettings.GetValue<string>("InstanceName");
 });
 
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-
 builder.Services.AddScoped<IStorageLogic<Note>, StorageLogic>();
 builder.Services.AddScoped<IValidator<Note>, Validator>();
 builder.Services.AddScoped<ICache, CachingItem>();
@@ -47,8 +42,12 @@ builder.Services.AddOptions<LogicArgs>()
     .BindConfiguration("Flags")
     .ValidateDataAnnotations();
 
-var app = builder.Build();
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
 
+
+var app = builder.Build();
 
 if (app.Environment.IsDevelopment())
 {
@@ -57,84 +56,6 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
-
-
-app.MapGet("api/v1/notes/{id}", async (long id, IStorageLogic<Note> logic, IValidator<Note> noteValidator) =>
-{
-    var isValidId = id >= 1;
-
-    if (!isValidId)
-        return Results.NotFound();
-
-    var note = await logic.Get(id);
-    var adaptedNote = note.Adapt<NoteDtoOut>();
-
-    return Results.Ok(adaptedNote);
-}).Produces<NoteDtoOut>()
-  .Produces(404);
-
-app.MapPost("api/v1/notes/add", async (NoteDto noteDto, IStorageLogic<Note> logic, IValidator<Note> noteValidator) =>
-{
-    var note = new Note() { Title = noteDto.Title, Content = noteDto.Content };
-
-    if (!noteValidator.Validate(note).IsValid)
-        return Results.UnprocessableEntity();
-
-    var addedNoteId = await logic.Add(note);
-
-    return Results.Created(new Uri("api/v1/notes/", UriKind.Relative), addedNoteId);
-}).Produces(201)
-  .Produces(422);
-
-app.MapPut("api/v1/notes/update", async (NoteDtoUpdate noteDtoUpdate, IStorageLogic<Note> logic, IValidator<Note> noteValidator) =>
-{
-    var note = new Note() { Id = noteDtoUpdate.Id, Title = noteDtoUpdate.Title, Content = noteDtoUpdate.Content };
-    var validationResult = noteValidator.Validate(note);
-    var isValidId = note.Id >= 1;
-
-    if (!validationResult.IsValid || !isValidId)
-        return Results.UnprocessableEntity(validationResult.Errors);
-
-    var updatedNote = await logic.Update(note);
-    var adaptedNote = updatedNote.Adapt<NoteDtoOut>();
-
-    return Results.Ok(adaptedNote);
-}).Produces<NoteDtoOut>()
-  .Produces(422);
-
-app.MapDelete("api/v1/notes/delete/{id}", async (long id, IStorageLogic<Note> logic, IValidator<Note> noteValidator) =>
-{
-    var isValidId = id >= 1;
-    if (!isValidId)
-        return Results.NotFound();
-
-    await logic.Delete(id);
-
-    return Results.NoContent();
-}).Produces(404)
-  .Produces(204);
-
-app.MapGet("api/v1/notes/list", async (ushort pageNumber, int itemsCount, IStorageLogic<Note> logic, IValidator<Note> noteValidator) =>
-{
-    var noteList = await logic.GetPage(pageNumber, itemsCount);
-
-    var page = noteList.Adapt<Page<NoteDtoOut>>();
-
-    return Results.Ok(page);
-}).Produces(200)
-  .Produces<Page<NoteDtoOut>>();
-
-app.MapPost("api/v1/notes/addTest", async (ushort count, IStorageLogic<Note> logic, IValidator<Note> noteValidator) =>
-{
-    foreach (var item in Enumerable.Range(0, count))
-    {
-        var note = new Note() { Title = item.ToString(), Content = Guid.NewGuid().ToString() };
-        _ = await logic.Add(note);
-    }
-
-    return Results.NoContent();
-
-}).Produces(201)
-  .Produces(422);
+app.MapControllers();
 
 app.Run();
