72d43d5d84500489fee2b04b0d37c696c4443a1b
Pupyshev Ilya <PupyshevIA@abdgtl.com>
2024-10-01 16:19:51 +0300

    Base template created

--- Abdt.Loyal.NoteSaver.Web/Client/Program.cs
+++ Abdt.Loyal.NoteSaver.Web/Client/Program.cs
@@ -1,14 +1,15 @@
 using Abdt.Loyal.NoteSaver.Web.Client;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using MudBlazor.Services;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
-builder.Services.AddHttpClient("Abdt.Loyal.NoteSaver.Web.ServerAPI", client => client.BaseAddress = new Uri(builder.HostEnvironment.BaseAddress));
+builder.Services.AddMudServices();
 
-// Supply HttpClient instances that include access tokens when making requests to the server project
+builder.Services.AddHttpClient("Abdt.Loyal.NoteSaver.Web.ServerAPI", client => client.BaseAddress = new Uri(builder.HostEnvironment.BaseAddress));
 builder.Services.AddScoped(sp => sp.GetRequiredService<IHttpClientFactory>().CreateClient("Abdt.Loyal.NoteSaver.Web.ServerAPI"));
 
 await builder.Build().RunAsync();
