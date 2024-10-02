146a7c3553ae14657c8f243b77da7dd56a7a7baa
Pupyshev Ilya <PupyshevIA@abdgtl.com>
2024-10-02 07:44:47 +0300

    Added shared classes

---
+++ Abdt.Loyal.NoteSaver.Web/Shared/Note.cs
@@ -0,0 +1,11 @@
+namespace Abdt.Loyal.NoteSaver.Web.Shared
+{
+    public class Note
+    {
+        public long Id { get; set; }
+        public string Title { get; set; }
+        public string Content { get; set; }
+        public DateTimeOffset CreatedAt { get; set; }
+        public DateTimeOffset UpdatedAt { get; set; }
+    }
+}
