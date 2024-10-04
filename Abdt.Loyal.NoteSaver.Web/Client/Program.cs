4841c80fb46fa7227b9289c27408f9586bf66f9c
Pupyshev Ilya <PupyshevIA@abdgtl.com>
2024-10-04 14:09:20 +0300

    Added update, delete methods

--- Abdt.Loyal.NoteSaver.Web/Client/Program.cs
+++ Abdt.Loyal.NoteSaver.Web/Client/Program.cs
@@ -13,7 +13,7 @@ builder.Services.AddMudServices();
 
 builder.Services.AddHttpClient<INoteClient<Note>, NoteHttpClient>(client =>
 {
-    client.BaseAddress = new Uri("https://localhost:50955/api/v1/notes/");
+    client.BaseAddress = new Uri("https://localhost:51332/api/v1/notes/");
 });
 
 await builder.Build().RunAsync();
